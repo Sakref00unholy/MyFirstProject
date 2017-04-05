c4e305937463b0c317bf59e00df16600fd1d7b6d
Sakref00unholy <phillipmayland@gmail.com>
2017-04-05 12:00:39 +0200

    first edition
    made the project and followed the instructions all the way to running
    the system.

---
+++ mobile/src/main/java/phillipmayland/myfirstproject/MainActivity.java
@@ -0,0 +1,13 @@
+package phillipmayland.myfirstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(Sakref.myfirstproject.R.layout.activity_main);
+    }
+}
